64e8aa50ff39d398981f5f2f302e9fce084e3a1c
sayrus89 <sayrus5767@mail.ru>
2021-08-27 17:30:14 +0500

    4.1.1. Операторы сравнения в String. [#237406]

---
+++ src/ru/job4j/condition/StringEq.java
@@ -0,0 +1,14 @@
+package ru.job4j.condition;
+
+public class StringEq {
+
+    public static void main(String[] args) {
+        String first = new String("one");
+        String second = new String("one");
+        if (first.equals(second)) {
+            System.out.println("first equals second");
+        } else {
+            System.out.println("first does not equals second");
+        }
+    }
+}
