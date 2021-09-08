9c001e327a3d631cd2ad897d469d2f4adc430423
sayrus89 <sayrus5767@mail.ru>
2021-09-08 23:55:47 +0500

    6.4. Слово начинается с ... [#41585 #200609] исправление.

--- src/main/java/ru/job4j/array/ArrayChar.java
+++ src/main/java/ru/job4j/array/ArrayChar.java
@@ -6,9 +6,10 @@ public class ArrayChar {
         boolean result = true;
         for (int i = 0; i < pref.length; i++) {
             if (word[i] != pref[i]) {
-                return false;
+                result = false;
+                break;
             }
         }
-        return true;
+        return result;
     }
 }
