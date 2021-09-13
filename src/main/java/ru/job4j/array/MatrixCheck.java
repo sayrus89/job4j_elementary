3f125f7ebdb2e173535f5f10cc3310d1f61497f2
sayrus89 <sayrus5767@mail.ru>
2021-09-13 16:50:16 +0500

    6.7.4. Выигрышные комбинации в сокобан [#53859]

--- src/main/java/ru/job4j/array/MatrixCheck.java
+++ src/main/java/ru/job4j/array/MatrixCheck.java
@@ -31,4 +31,15 @@ public class MatrixCheck {
         }
         return rsl;
     }
+
+    public static boolean isWin(char[][] board) {
+        boolean result = false;
+        for (int index = 0; index <= board.length / 2; index++) {
+            if (MatrixCheck.monoHorizontal(board, index) || MatrixCheck.monoVertical(board, index)) {
+                result = true;
+                break;
+            }
+        }
+        return result;
+    }
 }
