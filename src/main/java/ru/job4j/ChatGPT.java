89381fbb315a26215670ad5be2af1387d686781d
sayrus89 <sayrus5767@mail.ru>
2024-11-04 20:34:57 +0500

    GPT. Нейросети спешат на помощь. [#505156] 3

--- src/main/java/ru/job4j/ChatGPT.java
+++ src/main/java/ru/job4j/ChatGPT.java
@@ -3,7 +3,8 @@ package ru.job4j;
 public class ChatGPT {
 
     /**
-     *  найди ошибку в коде package ru.job4j.array;
+     * @param найди ошибку в коде package ru.job4j.array;
+     *
      * public class SortSelected {
      *     public static int[] sort(int[] data) {
      *         for (int i = 0; i < data.length; i++) {
@@ -23,7 +24,6 @@ public class ChatGPT {
      *         for (int i = 0; i < data.length; i++) {
      *             int min = MinDiapason.findMin(data, i, data.length - 1);
      *             int index = FindLoop.indexInRange(data, min, i, data.length - 1);
-     *             // Передаем индексы элементов, которые нужно поменять местами
      *             SwitchArray.swap(data, i, index);
      *         }
      *         return data;
